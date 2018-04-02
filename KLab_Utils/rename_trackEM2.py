c5f13bbb343a3061a9200c05dbd52b6f7ae142ec
Hanyu Li <hanyuli@uchicago.edu>
2018-04-02 17:58:53 -0500

    zfill

--- KLab_Utils/rename_trackEM2.py
+++ KLab_Utils/rename_trackEM2.py
@@ -8,11 +8,12 @@ import re
 def rename_trackEM2(f_in_dir, f_out_dir=None):
     ''' rename trackEM2 output to match *[index].tif '''
     get_index = lambda f: int(re.match(r'^.*/.*_z(?P<index>[0-9]+).(.*)$', f).groupdict('index')['index'])
+    #get_index = lambda f: int(re.match(r'^.*/.*_(?P<index>[0-9]+).(.*)$', f).groupdict('index')['index'])
     print(f_in_dir)
     f_list = glob.glob(os.path.join(f_in_dir, '*.tif*'))
     for f in f_list:
         #print(f, get_index(f))
-        new_name = os.path.join(f_in_dir, 'S_'+str(get_index(f)) +'.tif')
+        new_name = os.path.join(f_in_dir, 'S_'+str.zfill(str(get_index(f)), 4) +'.tif')
         print(new_name)
         os.rename(f,new_name)
 
